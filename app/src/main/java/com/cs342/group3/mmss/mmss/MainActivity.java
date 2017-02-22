d4c87af28906ccfa97e5f98975ae86fa5733c79a
New Build <swalde3@uic.edu>
2017-02-22 11:22:03 -0600

    20170222 @ 1116, manual push

--- app/src/main/java/com/cs342/group3/mmss/mmss/MainActivity.java
+++ app/src/main/java/com/cs342/group3/mmss/mmss/MainActivity.java
@@ -1,10 +1,7 @@
 package com.cs342.group3.mmss.mmss;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import layout.MMSS;
 
 public class MainActivity extends AppCompatActivity {
 
