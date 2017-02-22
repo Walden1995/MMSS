d63e275458cd7ce241b441aae4b442f51eefc731
New Build <swalde3@uic.edu>
2017-02-22 12:46:33 -0600

    L1247; added new page, new button to MainActivity. Possibly fixed the activity_main.xml and activity_page.xml

--- app/src/main/java/com/cs342/group3/mmss/mmss/MainActivity.java
+++ app/src/main/java/com/cs342/group3/mmss/mmss/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cs342.group3.mmss.mmss;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                goToSecondActivity();
+            }
+        });
     }
+
+    private void goToSecondActivity() {
+        Intent intent = new Intent(this, page.class);
+        startActivity(intent);
+    }
+
 }
